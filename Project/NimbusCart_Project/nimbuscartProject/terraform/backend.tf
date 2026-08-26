4812343ab024fb4bff79f466167dcd35ada49e46
Saumya Pandey <pandeyemail16@gmail.com>
2026-08-26 23:48:22 +0530

    Project

--- Project/NimbusCart_Project/nimbuscartProject/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "nimbuscart-tf-state-500692702174"
-    key            = "nimbuscart/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "nimbuscart-tf-lock"
-    encrypt        = true
-  }
-}
